6b7c259f93548e371b94635e2a40061d7e003d3a
Mario Alber <marioalberg1@gmail.com>
2024-06-11 18:38:51 +0200

    WIP

---
+++ rafi_gazebo/build/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/src/parser.cpp.o"
+  "devel/lib/rafi_gazebo/parser"
+  "devel/lib/rafi_gazebo/parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
